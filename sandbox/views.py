d4eafbdabad9b38f834a57eb5ab5958943c77a56
Darknight47 <farhadgunners@gmail.com>
2024-06-16 14:09:15 +0200

    created a view and the index.html file

--- sandbox/views.py
+++ sandbox/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 # Any view has to take a request *
 def helloWorld(request):
     return HttpResponse("Hello World! From the ("") path in the urls.py")
+
+def index(request):
+    # A view for dynamically calling the index.html
+    return render(request, "index.html")
