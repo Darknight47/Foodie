ad38db7cc2bca19cfd9ef5ed40e560939cf7236f
Darknight47 <farhadgunners@gmail.com>
2024-06-16 11:28:08 +0200

    gave a necessary request to the view's function

--- sandbox/views.py
+++ sandbox/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def helloWorld():
+# Any view has to take a request *
+def helloWorld(request):
     return HttpResponse("Hello World! From the ("") path in the urls.py")
