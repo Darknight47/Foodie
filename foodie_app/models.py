2e5a3d33bf88329e38aea3313f1a5624a7cad5d4
Darknight47 <farhadgunners@gmail.com>
2024-06-18 18:54:02 +0200

    registered and updated the Category view

--- foodie_app/models.py
+++ foodie_app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
+    date_added = models.DateField(auto_now_add=True)  # Capture the date & time when this category added
     def __str__(self):
         return self.name
