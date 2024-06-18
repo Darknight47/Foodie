559503064504679e753ad97eac7f183ab9e08fe3
Darknight47 <farhadgunners@gmail.com>
2024-06-18 19:01:19 +0200

    updated the registration of the category

--- foodie_app/admin.py
+++ foodie_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from . models import Category
+
+# A class for customizing the fields that we are seeing in the UI.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "date_added")
+    search_fields = ["name"] # Able to search by name
 # Register your models here.
-admin.site.register(Category)
+# registering the customization to the model.
+admin.site.register(Category, CategoryAdmin)
