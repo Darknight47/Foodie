126fbed4619c84c812432755b8b8fa6bbb9a93e9
Darknight47 <farhadgunners@gmail.com>
2024-06-18 20:08:34 +0200

    Create & add the recipe model

--- foodie_app/admin.py
+++ foodie_app/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from . models import Category
+from . models import Category, Recipe
 
 # A class for customizing the fields that we are seeing in the UI.
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "date_added")
     search_fields = ["name"] # Able to search by name
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "date_added")
+
 # Register your models here.
 # registering the customization to the model.
 admin.site.register(Category, CategoryAdmin)
+
+# registering the recipe model
+admin.site.register(Recipe, RecipeAdmin)
--- foodie_app/models.py
+++ foodie_app/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 # Create your models here.
+# A category can have one or many categories.
 class Category(models.Model):
     name = models.CharField(max_length=100)
     date_added = models.DateField(auto_now_add=True)  # Capture the date & time when this category added
+    
+    def __str__(self):
+        return self.name
+    
+# Relationship between Recipe and Category is ONE to MANY.
+
+# Recipe Model
+# Each recipe belongs to one category.
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # Everytime we delete a category, all the recipes belong to that category deletes as well!
+
     def __str__(self):
         return self.name
