ffaf9bc36d68c96e3f44536546efd66508b4e55a
Darknight47 <farhadgunners@gmail.com>
2024-06-19 13:28:31 +0200

    registered the model in the 'comments' app

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from comments.models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "date_added")
+
 # Register your models here.
+
+admin.site.register(Comment, CommentAdmin)
