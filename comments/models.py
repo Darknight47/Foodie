c81be27c8f31a000f5c8d29576acd2527b2374b1
Darknight47 <farhadgunners@gmail.com>
2024-06-19 13:19:11 +0200

    added a url and the comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from recipes.models import Recipe
+
 # Create your models here.
+# Each Recipe can have ZERO to MANY comments.
+
+class Comment(models.Model):
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    # Everytime a Recipe gets deleted, its comments always get deleted!
+
+    def __str__(self):
+        return self.text
