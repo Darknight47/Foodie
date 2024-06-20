87acceb363ce86b0385f2efeb57086a8d6c6aad7
Darknight47 <farhadgunners@gmail.com>
2024-06-20 17:54:53 +0200

    showing recipes under each category

--- foodie_app/urls.py
+++ foodie_app/urls.py
@@ -3,5 +3,6 @@ from . import views # Importing views from the current directory
 
 app_name = "foodie_app"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("recipes/<int:category_id>/", views.catRecipes, name="cat_recipes")
 ]
--- foodie_app/views.py
+++ foodie_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from . models import Category
+from recipes.models import Recipe
 
 # Create your views here.
 def index(request):
     #Inside the foodie_app execute the index.html
-    return render(request, "foodie_app/index.html")
+    categories = Category.objects.all()
+    context = {"categories": categories}
+
+    return render(request, "foodie_app/index.html", context=context)
+
+
+def catRecipes(request, category_id):
+    recipies = Recipe.objects.filter(category=category_id)
+    category = Category.objects.get(id=category_id)
+    context = {'catRecipes': recipies,
+               'category': category}
+    
+    return render(request, "foodie_app/catrecipes.html", context=context)
