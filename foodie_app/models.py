459b1adb81ce1fe6b391693fc89ec59d3f4eb3b9
Darknight47 <farhadgunners@gmail.com>
2024-06-20 21:45:00 +0200

    specifying meta info about the category model

--- foodie_app/models.py
+++ foodie_app/models.py
@@ -6,5 +6,13 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     date_added = models.DateField(auto_now_add=True)  # Capture the date & time when this category added
     
+    # Changing or specifying information about this model.
+    # Specyfing Meta Options.
+    class Meta: 
+        ordering = ["-name"] # For descending order of the name property. "name" for ascending ordering.
+        #ordering = ["name", "date_added"] # Ordering by two properties.
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
