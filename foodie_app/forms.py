d9242eb2831bd88622726be2bcfc600afa7e4f83
Darknight47 <farhadgunners@gmail.com>
2024-06-21 16:14:29 +0200

    added forms.py for creating a category form

---
+++ foodie_app/forms.py
@@ -0,0 +1,15 @@
+
+from django import forms
+
+from foodie_app.models import Category
+
+
+# A form for creating Category from UI.
+class CategoryForm(forms.Form):
+    class Meta: #Defining meta data about this form
+            # We need to attach this category form to a model.
+            model = Category
+            fields = ["name"] #User only need to write the name of a category for creating one.
+            labels = {
+                  'name': 'Category Name'
+            }
